e15510f8483c418c8ddb59bfbf9b3f4b4cb4799b
JonCass <JCass1980@gmail.com>
2017-03-13 15:36:44 -0700

    java coming together

--- main.js
+++ main.js
@@ -2,23 +2,25 @@ $(document).ready(function(){
 	$.getJSON('http://json-data.herokuapp.com/forms', function(formData){
 		var formHTML = ""
 
-		formData.forEach(function(field){
-		formHTML += `<input type="${field.type}" placeholder=" ${field.label}" id="${field.id}" />`
-		})
-
-		$("form").html(formHTML)
+		
 
 		formData.forEach(function(field){
 			if (field.type == "select"){
 				selectArr = []
 				selectArr[0] = "<select>"
+				selectArr += `<option value = "none selected">Select Language...</option>`
 				field.options.forEach(function(option){
 					selectArr += `<option value="${option.value}">"${option.label}"</option>`
 				})
 				selectArr += ('</select>')
-				console.log(selectArr)
+				formHTML += selectArr
+				}
+			else{
+				formHTML += `<input type="${field.type}" placeholder=" ${field.label}" id="${field.id}" />`	
 			}
 		})
+		
+		$("form").html(formHTML)
 	})
 })
 
