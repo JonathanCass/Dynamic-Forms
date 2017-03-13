8c7cefc82f3a51e909495b0143ec735885a28097
JonCass <JCass1980@gmail.com>
2017-03-13 15:17:22 -0700

    first sign of progress

--- main.js
+++ main.js
@@ -7,7 +7,28 @@ $(document).ready(function(){
 		})
 
 		$("form").html(formHTML)
+
+		formData.forEach(function(field){
+			if (field.type == "select"){
+				selectArr = []
+				selectArr[0] = "<select>"
+				field.options.forEach(function(option){
+					selectArr += `<option value="${option.value}">"${option.label}"</option>`
+				})
+				selectArr += ('</select>')
+				console.log(selectArr)
+			}
+		})
 	})
 })
 
 //<i class="fa ${field.icon}" aria-hidden="true"></i>
+//<i class="fa ${field.icon}" aria-hidden="true"></i>
+//[add(option[,index])]	Adds an <option> element into the collection at the specified index. If no index is specified, it inserts the option at the end of the collection
+/*
+<select>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>*/
